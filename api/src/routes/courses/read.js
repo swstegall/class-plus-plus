ea3a42ad4f98830e0a726e1091c3014c21021840
Sam Stegall <stegall@tamu.edu>
2021-10-30 16:02:21 -0500

    Setup teacher read own.

--- api/src/routes/courses/read.js
+++ api/src/routes/courses/read.js
@@ -1,5 +1,22 @@
+const verifyToken = require("../../utilities/verifyToken");
+const models = require("../../models");
+
 const read = async (req, res) => {
-  res.status(200).json({ success: true, message: "courses/read stub." });
+  try {
+    const tokenObject = verifyToken(req.headers.authorization.substring(7));
+    if (tokenObject.RoleID === 1) {
+      const courses = await models.course.findAll({
+        where: { CreatedByUserID: tokenObject.UserID },
+      });
+      res.status(200).json(courses);
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "You are not a teacher." });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
 };
 
 module.exports = read;
--- sql/4_courses.sql
+++ sql/4_courses.sql
@@ -5,7 +5,9 @@ create table courses (
   Label varchar(255) not null,
   Title varchar(255) not null,
   Description varchar(255) not null,
-  constraint courses_ID_uindex unique (ID)
+  CreatedByUserID char(36) not null,
+  constraint courses_ID_uindex unique (ID),
+  constraint courses_users_ID_fk foreign key (CreatedByUserID) references users (ID)
 );
 
 alter table

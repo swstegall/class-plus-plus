ee01a7746a147e0874811b5190b26f07b0b21361
Sam Stegall <stegall@tamu.edu>
2021-10-31 00:53:17 -0500

    Setup read.

--- api/src/routes/assignments/read.js
+++ api/src/routes/assignments/read.js
@@ -1,5 +1,22 @@
+const verifyToken = require("../../utilities/verifyToken");
+const models = require("../../models");
+
 const read = async (req, res) => {
-  res.status(200).json({ success: true, message: "assignments/read stub." });
+  try {
+    const tokenObject = verifyToken(req.headers.authorization.substring(7));
+    if (tokenObject.RoleID === 1 || tokenObject.RoleID === 0) {
+      const assignments = await models.assignment.findAll({
+        where: { CourseID: req.body.CourseID },
+      });
+      res.status(200).json(assignments);
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "You are not a teacher or student." });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
 };
 
 module.exports = read;
